10d0d4d87a019e805865d6858e255e18752c70bc
Adrian Martin <adrianmartinparra@gmail.com>
2012-10-17 16:14:46 +0200

    Discarded threads until needed.
    Robot execution is no longer threaded. Single robot assumed.

--- ebots/robotics.py
+++ ebots/robotics.py
@@ -1,6 +1,6 @@
 from __future__ import division
+from collections import defaultdict
 
-import threading
 import morsesim
 import time
 
@@ -9,27 +9,12 @@ class RoboticsError(Exception):
     pass
 
 
-class IntervalRunner(threading.Thread):
-    def __init__(self, interval, steps, func, *args, **kwargs):
-        self.interval = interval
-        self.steps = steps
-        self.func = func
-
-        self.kwargs = kwargs
-        self.args = args
-
-        super(IntervalRunner, self).__init__()
-
-    def run(self):
-        for i in range(self.steps):
-            self.func(*self.args, **self.kwargs)
-            time.sleep(self.interval)
-
-
 class BaseRobot(object):
     def __init__(self, components, name='dummy', host='localhost'):
-        self.actuators = []
-        self.sensors = []
+        self.components = components
+
+        self.actuators = defaultdict(list)
+        self.sensors = defaultdict(list)
         self.name = name
         self.host = host
 
@@ -37,61 +22,55 @@ class BaseRobot(object):
 
     def _create_devices(self, components):
         for name, port in sorted(components.items()):
-            kind, sid, rid = name.split('_')
+            kind, type, did, rid = name.split('_')
             device = getattr(morsesim, kind.title())
 
             if device is morsesim.Actuator:
-                self.actuators.append(device(name, port, self.host))
+                self.actuators[type].append(device(name, port, self.host))
             elif device is morsesim.Sensor:
-                self.sensors.append(device(name, port, self.host))
+                self.sensors[type].append(device(name, port, self.host))
             else:
                 msg = 'device {0} does not exists'.format(device)
                 raise RoboticsError(msg)
-
-    def _execute(self):
-        '''
-        Base clases must implement this method.
-        '''
-        pass
-
-    def move(self, steps, interval):
-        thread = IntervalRunner(interval, steps, self._execute)
-        thread.start()
 
 
 class NeuralRobot(BaseRobot):
     def __init__(self, components, network, name='dummy', host='localhost'):
         self.network = network
-        super(NeuralRobot, self).__init__(components, name, host)
-
-
-class ValentinoRobot(NeuralRobot):
-    def _execute(self):
-        input = [min(sensor.read()['range_list']) for sensor in self.sensors]
-        out0, out1 = self.network.eval(input)
-
-        velocity = (out0 + out1) / 2
-        omega = (out0 - out1)
-
-        actuator = self.actuators[0]
-        actuator.write({'v': velocity, 'w': omega})
-
-        print(actuator.name, velocity, omega)
+        super(self.__class__, self).__init__(components, name, host)
 
 
 class EnergyRobot(NeuralRobot):
     def __init__(self, components, network, name='dummy', host='localhost'):
+        self.cookies = {}
         self.energy = 0
         super(self.__class__, self).__init__(components, network, name, host)
 
-    def _execute(self):
-        input = [min(sensor.read()['range_list']) for sensor in self.sensors]
-        out0, out1 = self.network.eval(input)
+    def move(self, steps, interval):
+        self.cookies = {}
+        self.energy = 0
 
-        velocity = (out0 + out1) / 2
-        omega = (out0 - out1)
+        proximity = self.sensors['proximity'][0]
+        waypoint = self.actuators['waypoint'][0]
+        motion = self.actuators['motion'][0]
+        rangers = self.sensors['ranger']
 
-        actuator = self.actuators[0]
-        actuator.write({'v': velocity, 'w': omega})
+        for i in range(steps + 30):
+            input = [min(ranger.read()['range_list']) for ranger in rangers]
+            velocity, omega = self.network.eval(input)
 
-        print(actuator.name, velocity, omega)
+            if i < 30:
+                motion.write({'v': 0, 'w': 0})
+                waypoint.write({'x': 0, 'y': 0, 'z': 0, 'tolerance': 0.5, 'speed': 5})
+
+            else:
+                cookies = proximity.read()['near_objects']
+                if set(cookies).difference(set(self.cookies)):
+                    self.energy += 1
+                self.cookies.update(cookies)
+
+                waypoint.write({'x': 0, 'y': 0, 'z': 0, 'tolerance': 0.5, 'speed': 0})
+                motion.write({'v': velocity, 'w': omega})
+
+            time.sleep(interval)
+            #print(motion.name, velocity, omega, self.energy)
