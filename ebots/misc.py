3230eed1845db11393c5df922b84f7b646e043e5
Adrian Martin <adrianmartinparra@gmail.com>
2012-10-18 21:15:35 +0200

    Added axes support to "hinton".

--- ebots/misc.py
+++ ebots/misc.py
@@ -12,7 +12,7 @@ def generalized_logistic(t, A, B, K, M, Q, nu):
     return A + ((K - A) / ((1 + Q * numpy.exp(-B * (t - M))) ** (1 / nu)))
 
 
-def _blob(x, y, area, colour):
+def _blob(x, y, area, colour, ax):
     '''
     Draws a square-shaped blob with the given area (< 1) at
     the given coordinates. https://gist.github.com/292018
@@ -20,28 +20,29 @@ def _blob(x, y, area, colour):
     hs = numpy.sqrt(area) / 2
     xcorners = numpy.array([x - hs, x + hs, x + hs, x - hs])
     ycorners = numpy.array([y - hs, y - hs, y + hs, y + hs])
-    plt.fill(xcorners, ycorners, colour, edgecolor=colour)
+    ax.fill(xcorners, ycorners, colour, edgecolor=colour)
 
 
-def hinton(W, maxweight=None):
+def hinton(W, maxweight=None, ax=None):
     '''
     Draws a Hinton diagram for visualizing a weight matrix.
     Temporarily disables matplotlib interactive mode if it is on,
     otherwise this takes forever. https://gist.github.com/292018
     '''
 
-    plt.figure()
+    if ax is None:
+        fig, ax = plt.subplots()
+
     height, width = W.shape
 
     if not maxweight:
         maxweight = 2 ** numpy.ceil(numpy.log(numpy.max(numpy.abs(W))) / numpy.log(2))
 
-    plt.fill(numpy.array([0, width, width, 0]),
-             numpy.array([0, 0, height, height]),
-             'gray')
+    ax.fill(numpy.array([0, width, width, 0]),
+             numpy.array([0, 0, height, height]), 'gray')
 
-    plt.axis('off')
-    plt.axis('equal')
+    ax.axis('off')
+    ax.axis('equal')
 
     for x in range(width):
         for y in range(height):
@@ -50,6 +51,6 @@ def hinton(W, maxweight=None):
             w = W[y, x]
 
             if w > 0:
-                _blob(_x - 0.5, height - _y + 0.5, min(1, w / maxweight), 'white')
+                _blob(_x - 0.5, height - _y + 0.5, min(1, w / maxweight), 'white', ax)
             elif w < 0:
-                _blob(_x - 0.5, height - _y + 0.5, min(1, -w / maxweight), 'black')
+                _blob(_x - 0.5, height - _y + 0.5, min(1, -w / maxweight), 'black', ax)
